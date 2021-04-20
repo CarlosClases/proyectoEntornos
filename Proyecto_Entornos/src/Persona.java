3dbe7097aa56a6da684f2e53b00d3dc1b1fcc05a
CarlosClases <cleivaclases@gmail.com>
2021-04-20 11:26:49 +0200

    Version 2: Ya no se pueden repetir nombres de usuario

--- Proyecto_Entornos/src/Persona.java
+++ Proyecto_Entornos/src/Persona.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Persona {
@@ -7,11 +8,31 @@ public class Persona {
 													   "informatica", "series", "peliculas"};
 	private String user_name;
 	private boolean [] gustos_personales = new boolean[6];
+
+	private static ArrayList<String>  user_name_no_disponibles = new ArrayList<String>();
 	
 	public Persona (String Nombre_introducido){
-		this.user_name = Nombre_introducido;
+		if(!user_name_no_disponibles.contains(Nombre_introducido)) {
+			user_name_no_disponibles.add(Nombre_introducido);
+			this.user_name =  Nombre_introducido;
+		}
 	}
 	
+
+	public static ArrayList<String> getUser_name_no_disponibles() {
+		return user_name_no_disponibles;
+	}
+
+
+	public String getUser_name() {
+		return user_name;
+	}
+
+
+	public String toString() {
+		return "Persona [user_name=" + user_name + "]";
+	}
+
 	public void PersonalizarGustos() {
 		System.out.println("¿Te gusta el futbol? SI/NO");
 	}
