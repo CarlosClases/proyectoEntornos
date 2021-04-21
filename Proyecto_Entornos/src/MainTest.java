f959dfb05d2ff3ddff48735e1a5c41df2b76bd76
gonza <gonza@portatilDANI>
2021-04-21 15:51:44 +0200

    version6.1
    Ahora creo que si se ha subido el main
    
    
    Signed-off-by: gonza <gonza@portatilDANI>

---
+++ Proyecto_Entornos/src/MainTest.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MainTest {
+	public static Scanner tec = new Scanner(System.in);
+	public static void main(String[] args) {
+		
+		ArrayList<Persona> listaPersonas = new ArrayList<Persona>();
+		
+		boolean [] b = {true ,true ,true ,true ,true ,false, false};
+		Persona a = new Persona("administrador", b, listaPersonas);
+		
+		new Persona(listaPersonas);
+		new Persona(listaPersonas);
+
+		
+		System.out.println(listaPersonas.get(0));
+		System.out.println(listaPersonas.get(1));
+
+	}
+}
